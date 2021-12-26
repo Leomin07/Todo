15d0b18cc1f8a2fc0d589b29f265b50901273313
Leomin07 <tranminhsvp@gmail.com>
2021-12-26 21:41:40 +0700

    init commit

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -2,7 +2,9 @@ import { connect } from 'mongoose';
 
 const run = async (): Promise<void> => {
   try {
-    await connect('mongodb://localhost:27017/todo');
+    await connect(
+      'mongodb+srv://root:123@cluster0.cq7uq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    );
     console.log('connected');
   } catch (error) {
     console.log(error);
